764fb3ae4490cfd78e63e73b378bf645b9fc1ade
ambrella <504489929@qq.com>
2016-10-31 15:26:22 +0800

    10

--- helloWorld/HelloWorld.java
+++ helloWorld/HelloWorld.java
@@ -1,10 +1,12 @@
 package helloWorld;
 // package heloworld
-
+import arrayUtils.*;
 public class HelloWorld {
 
 	public static void main(String []args){
 		//public static void main(string []args)
+		int[] array={1,5,6,7,1,6,8,2,3,9,2};
+		Arrayutils.printArray(array);
 		System.out.println("hello world");
 	}
 }
